ad0c5981b775cc29f978c4dcd4e554d3222773a4
Stojancho Sirachevski <sirachevski.stojancho@gmail.com>
2018-06-15 22:20:36 +0100

    Create migration, model and controller.

--- database/migrations/2018_06_15_210606_create_records_table.php
+++ database/migrations/2018_06_15_210606_create_records_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateRecordsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,15 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('records', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
+            $table->string('first_name');
+            $table->string('last_name');
             $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('phone_number');
+            $table->char('gender', 1);
+            $table->date('dob');
+            $table->text('comments');
             $table->timestamps();
         });
     }
@@ -30,6 +33,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('records');
     }
 }
